0c637eb0b1f8eaff18db2f70510ad69ba8870dfe
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-20 15:25:34 -0500

    built navbar + router

---
+++ src/components/NavTabs/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavTabs() {
+  return(
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+      <Link to="/" className="navbar-brand py-2">
+        Garrett Roberts
+      </Link>
+
+      <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navBarToggler">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navBarToggler">
+      <ul className="navbar-nav">
+        <li className="nav-item px-3">
+            <Link to="/about" className="nav-item px-3 nav-link">
+              About
+            </Link>
+        </li>
+        <li className="nav-item px-3">
+            <Link to="/portfolio" className="nav-item px-3 nav-link">
+              Portfolio
+            </Link>
+        </li>
+        <li className="nav-item px-3">
+          <Link to="/contact" className="nav-item px-3 nav-link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  )
+};
+
+export default NavTabs;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Portfolio() {
+  return (
+    <h1>Portfolio Page</h1>
+  );
+}
+
+export default Portfolio;
